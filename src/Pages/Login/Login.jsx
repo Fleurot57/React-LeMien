51d6370cefdd7aaa0ca654307c3ab8a003bb6a6f
Damien <fleurot.damien197@gmail.com>
2022-10-19 11:22:57 +0200

    modif login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,16 +1,18 @@
 import "./Login.css";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 //Damien
 //Formulaire login: input(mail), input(mdp)
 //Boutton : Login
 
 function Login() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const Navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -23,21 +25,26 @@ function Login() {
       },
       body: JSON.stringify(item),
     };
-    let result = await toast.promise(
+    let result = await 
       fetch(
         "https://social-network-api.osc-fr1.scalingo.io/lemien/login",
         options
-      ),
-      {
-        pending: "Promise is pending",
-        success: "Promise resolved 👌",
-        error: "Promise rejected 🤯",
-      }
-    );
+      );
 
-    console.log(result);
     result = await result.json();
+    console.log(result);
     localStorage.setItem("token", result.token);
+
+      if (result.success) {
+        setEmail("");
+        setPassword("");
+        setMessage("Connecter avec succée");
+        setTimeout(() => { Navigate("/"); }, 3000);
+        toast.info("Connexion reussi, Redirection")
+
+      } else {
+        setMessage("Une erreur s'est produite");
+      }
 
 }
 
@@ -72,6 +79,7 @@ function Login() {
             <button type="submit" className="login-btn">
               Connexion
             </button>
+            <div className="message">{message ? <p>{message}</p> : null}</div>
           </form>
           <div id="login-form-bottom">
             <p className="mb-2 login-account">Tu as un compte ?</p>
@@ -82,7 +90,7 @@ function Login() {
           </div>
         </div>
       </div>
-      <ToastContainer />
+      < ToastContainer autoClose = { 3000 } />  
     </section>
   );
 }
